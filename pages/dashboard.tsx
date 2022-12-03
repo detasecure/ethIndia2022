a0c86fc34571cf48a5e52e980cd73fc3b770d697
Pratham Bhatnagar <prathambhatnagar2803@gmail.com>
2022-12-04 03:26:39 +0530

    lets buidl it

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import CardBarChart from '../components/dashboard/CardBarChart'
 import RadarChart from '../components/dashboard/RadarChart'
-import Stats from '../components/dashboard/Stats'
 import WalletList from '../components/dashboard/WalletList'
+import Stats from "../components/dashboard/Stats";
+import _data from "../components/dashboard/sample.json";
 
 const dashboard = () => {
   const [loaded, setLoaded] = useState(false);
@@ -12,14 +13,33 @@ const dashboard = () => {
   }, []);
 
   return (
-    <div className='grid grid-cols-3 items-center justify-center w-[70%] p-10 gap-4 mx-auto'>
-      {loaded ? <RadarChart /> : ""}
-      <div className='col-span-2'>
-      {loaded ? <WalletList /> : ""}
+    <>
+      <h1 className="my-4 font-bold text-6xl text-center">
+        {_data.data.organization}
+      </h1>
+      <div className="flex justify-center my-6">
+        <a href={"https://twitter.com/coinbase"}>
+          <img
+            className="w-10 h-10"
+            src="https://img.freepik.com/free-icon/twitter_318-804659.jpg?t=st=1670098564~exp=1670099164~hmac=b40678e87b12d94a0fca4d68266d62b58a5d7f5525c014938d4b60eee23a6db9"
+          />
+        </a>
       </div>
-      {loaded ? <CardBarChart /> : ""}
-      {loaded ? <Stats value={24} /> : ""}
-    </div>
+      <div className="flex gap-4 my-5 justify-center">
+        {" "}
+        <div className="rounded-xl bg-pink-400 px-5 py-3 cursor-pointer">
+          Subscribe with Push
+        </div>
+        <div className="rounded-xl bg-indigo-500 px-5 py-3 cursor-pointer">
+          {" "}
+          See Details
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 items-center  h-screen ">
+        {loaded ? <Stats /> : ""}
+        {/* {loaded ? <Charts /> : ""} */}
+      </div>
+    </>
   );
 };
 
